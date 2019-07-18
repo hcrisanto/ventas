6df82ec187cfd8a39a7cf51137b43c61e95ccbd2
Crisanto <Crisanto@DESKTOP-HE3HNPN>
2019-07-18 00:58:15 -0600

    clase ciudad y cliente

---
+++ Ventas/Ventas/Cliente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ventas
+{
+    public class Cliente
+    {
+       
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public string Ciudad { get; set; }
+        public string Direcion { get; set; }
+
+        public Cliente(int id, string nombre, string tel,string ciudad, string direccion)
+        {
+            Id = id;
+            Nombre = nombre;
+            Telefono = tel;
+            Ciudad = ciudad;
+            Direcion = direccion;
+        }
+    }
+
+    }
+    
---
+++ Ventas/Ventas/Form2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ventas
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+            var cliente1 = new Cliente(001,"Pedro","90899878","San Pedro Sula,", "El Pedregal");
+            var cliente2 = new Cliente(002, "Salomon","90901221", "Tegucigalpa,", "San Francisco");
+            var cliente3 = new Cliente(003, "Teresa", "98978900 ", "El Progreso,", "Altos de Progreso");
+
+            var cliente = new List<Cliente>();
+            cliente.Add(cliente1);
+            cliente.Add(cliente2);
+            cliente.Add(cliente3);
+
+
+
+            foreach (var Clientes in cliente)
+            
+                MessageBox.Show(Clientes.Id + " " + Clientes.Nombre + " " +Clientes.Telefono+ " "+ Clientes.Ciudad +" "+Clientes.Direcion );
+
+            }
+
+        }
+    }
+
