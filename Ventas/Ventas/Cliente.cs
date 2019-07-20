c63cfecd9e05aa98dad7a0a46d678fe6ad4599c9
Crisanto <Crisanto@DESKTOP-HE3HNPN>
2019-07-20 12:02:16 -0600

    clase clientes y ciudad (Acttulizadas)

--- Ventas/Ventas/Cliente.cs
+++ Ventas/Ventas/Cliente.cs
@@ -8,21 +8,22 @@ namespace Ventas
 {
     public class Cliente
     {
-       
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Telefono { get; set; }
-        public string Ciudad { get; set; }
-        public string Direcion { get; set; }
+        public Ciudad Ciudad { get; set; }
+        public string Direccion { get; set; }
 
-        public Cliente(int id, string nombre, string tel,string ciudad, string direccion)
+        public Cliente(int id, string nombre, string tel, Ciudad ciudad, string direccion)
         {
             Id = id;
             Nombre = nombre;
             Telefono = tel;
             Ciudad = ciudad;
-            Direcion = direccion;
+            Direccion = direccion;
         }
+
+       
     }
 
     }
--- Ventas/Ventas/Form2.cs
+++ Ventas/Ventas/Form2.cs
@@ -20,10 +20,16 @@ namespace Ventas
         private void button1_Click(object sender, EventArgs e)
 
         {
-            var cliente1 = new Cliente(001,"Pedro","90899878","San Pedro Sula,", "El Pedregal");
-            var cliente2 = new Cliente(002, "Salomon","90901221", "Tegucigalpa,", "San Francisco");
-            var cliente3 = new Cliente(003, "Teresa", "98978900 ", "El Progreso,", "Altos de Progreso");
+            var ciudad1 = new Ciudad(23201, "El Progreso");
+            var ciudad2 = new Ciudad(21101, "San Pedro Sula");
+            var ciudad3 = new Ciudad(11101, "Tegucigalpa");
 
+
+            var cliente1 = new Cliente(001,"Pedro","90899878",ciudad2, "El Pedregal");
+            var cliente2 = new Cliente(002, "Salomon","90901221", ciudad3, "San Francisco");
+            var cliente3 = new Cliente(003, "Teresa", "98978900 ", ciudad1, "Altos de Progreso");
+
+           
             var cliente = new List<Cliente>();
             cliente.Add(cliente1);
             cliente.Add(cliente2);
@@ -33,7 +39,7 @@ namespace Ventas
 
             foreach (var Clientes in cliente)
             
-                MessageBox.Show(Clientes.Id + " " + Clientes.Nombre + " " +Clientes.Telefono+ " "+ Clientes.Ciudad +" "+Clientes.Direcion );
+                MessageBox.Show(Clientes.Id + " " + Clientes.Nombre + " " +Clientes.Telefono+ " "+ Clientes.Ciudad.Descripcion +", "+Clientes.Direccion );
 
             }
 
